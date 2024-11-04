207eee9c6544b33dc0ad7640647140987a32e25c
merve-celik16 <mervedursunn34@gmail.com>
2024-11-04 23:10:45 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool sayi=false;
+            while (!sayi)
+            {
+                try
+                {
+                    Console.WriteLine(" Lütfen sayı giriniz:");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    int sonuc = number * number;
+                    Console.WriteLine(sonuc);
+                    sayi = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Hatalı işlem yaptınız");
+                }
+                finally 
+                {
+                    Console.WriteLine("İşlem tamamlandı.");
+                }
+            }
+
+        }
+    }
+}
